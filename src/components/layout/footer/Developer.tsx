96ccb34c450c2ce56a8dba66148a7fce662ab201
landeholt <johnlan@kth.se>
2021-08-24 14:44:49 +0200

    added vercel text badge

--- src/components/layout/footer/Developer.tsx
+++ src/components/layout/footer/Developer.tsx
@@ -1,8 +1,33 @@
 import useTranslation from "next-translate/useTranslation";
-import { Text } from "@chakra-ui/layout";
-import React from "react";
+import { Text, VStack } from "@chakra-ui/layout";
+import React, { useState } from "react";
+import AccessibleLink from "components/AccessibleLink";
+import { chakra } from "@chakra-ui/system";
 
 export const Developer = () => {
     const { t } = useTranslation("common");
-    return <Text>{t("footer.developer")}</Text>;
+
+    const [isHover, setHover] = useState(false);
+    return (
+        <VStack spacing={4}>
+            <Text>{t("footer.developer")}</Text>
+            <Text>
+                <AccessibleLink
+                    href="https://vercel.com"
+                    isExternal
+                    textDecoration="none"
+                    onMouseEnter={() => setHover(true)}
+                    onMouseLeave={() => setHover(false)}
+                >
+                    Powered by{" "}
+                    <chakra.span
+                        color={isHover ? "#0070F3" : "#000"}
+                        transition="all 0.2s ease-in"
+                    >
+                        ▲ Vercel
+                    </chakra.span>{" "}
+                </AccessibleLink>
+            </Text>
+        </VStack>
+    );
 };

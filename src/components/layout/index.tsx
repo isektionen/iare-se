63d281f1ec9ee03be30043dd665b77c4e1296331
landeholt <johnlan@kth.se>
2021-11-10 11:44:35 +0100

    removed meta from index

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from "react";
 
 import Header from "./Header";
 import Footer from "./Footer";
-import Meta from "./Meta";
 import { Box, Container, Flex, VStack } from "@chakra-ui/react";
 import { AlertSection } from "./AlertSection";
 
@@ -18,7 +17,6 @@ const Layout = (props: Props) => {
             position="relative"
         >
             <Header px={{ base: 4, md: 12 }} />
-            <Meta />
             <Box bg="white" w="100vw" pt={20}>
                 {props.children}
             </Box>

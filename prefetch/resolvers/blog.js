f279a27753e1d830fac3d273a73a5d9b31ab5234
Oliver Midbrink <oliver.midbrink@hotmail.com>
2021-11-24 18:09:02 +0100

    Added function that selects the backup data if no data is available from cms.

--- prefetch/resolvers/blog.js
+++ prefetch/resolvers/blog.js
@@ -1,10 +1,13 @@
 const axios = require("axios");
+const backup_data = require('../backup_data/post.json');
+
 const resolver = async () => {
     const { data } = await axios.get("https://cms.iare.se/posts/");
+    const prefetch_data = data.length != 0 ? data[0] : backup_data;
 
     return {
         basename: "blog",
-        data,
+        data: prefetch_data,
     };
 };
 
